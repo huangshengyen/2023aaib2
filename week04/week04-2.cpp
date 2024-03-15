ca6f676afdd5f70461661e713d2dea524ab8c848
Huangshengyen <0966549066sam@gmail.com>
2024-03-15 10:25:21 +0800

    week04

---
+++ week04/week04-2.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+    //for (int i = 0; i<3; i++)
+    for (int i = 1; i<=3; i++){
+    printf("i¬O:%d\n",i);
+    }
+
+
+}
---
+++ week04/week04-2.py
@@ -0,0 +1,3 @@
+#week04-2 for 迴圈進階型 從1開始 python版
+for i in range(1,3+1):
+  print('i是',i)
